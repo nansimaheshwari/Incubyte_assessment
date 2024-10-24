3831e31a713fb7f9b08147f78f3bc3df07bde579
Nansi Maheshwari <nansi.maheshwari@intimetec.com>
2024-10-24 20:55:41 +0530

    Custom delimeter functionality added

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 function add(numbers) {
-    if (numbers === "") return 0;
-    const numsArray = numbers.split(/[\n,]+/);
-    const sum = numsArray.reduce((acc, curr) => acc + parseInt(curr), 0);
-    return sum;
+  if (numbers === "") return 0;
+  let delimiter = /[\\n,]+/;
+
+  if (numbers.startsWith("//")) {
+    const newlineIndex = numbers.indexOf("\\n");
+    let customDelimiter = "";
+
+    if (newlineIndex !== -1) {
+      customDelimiter = numbers.slice(2, newlineIndex);
+      numbers = numbers.slice(newlineIndex + 2);
+      console.log(customDelimiter, numbers);
+    }
+    delimiter = new RegExp(`${customDelimiter}|${delimiter}`, "g");
+    console.log(delimiter, "f");
   }
-  
+
+  const numsArray = numbers.split(delimiter);
+  console.log(numsArray);
+  const sum = numsArray.reduce((acc, curr) => acc + parseInt(curr), 0);
+  return sum;
+}
+
 function calculate() {
   const input = document.getElementById("numbers").value;
   const result = add(input);
